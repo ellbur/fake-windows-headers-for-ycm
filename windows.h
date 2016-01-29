a1c502f003837a6f57153daf6220135183414378
Owen Healy <owen@pulsetor.com>
2016-01-29 18:59:56 -0500

    Some more

--- windows.h
+++ windows.h
@@ -8,3 +8,13 @@ typedef int HANDLE;
 HANDLE CreateMutex(void*, bool, void*);
 uintptr_t _beginthread(void(*start_address)(void *), unsigned, void*);
 
+void Sleep(int);
+void SetEvent(HANDLE);
+HANDLE CreateEvent(void*, bool, bool, void*);
+
+#define INFINITE 0
+#define FALSE 0
+#define TRUE 1
+
+void WaitForSingleObject(HANDLE, int);
+
